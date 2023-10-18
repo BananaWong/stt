4a30d9deaf0c15c7a5f915c6701bc311e2f0a993
BananaWong <bananawongh@gmail.com>
2023-10-18 10:59:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ async function uploadAudio() {
     const audioFile = document.getElementById('audioFile').files[0];
     const language = document.getElementById('language').value;
 
+
     if (!audioFile) {
         alert('请先选择一个音频文件！');
         return;
     }
 
     const audioData = new Blob([audioFile], { type: audioFile.type });
-    const AZURE_API_ENDPOINT = 'eastasia.api.cognitive.microsoft.com/sts/v1.0/issuetoken';
-    const response = await fetch(AZURE_API_ENDPOINT, {
+   // const AZURE_API_ENDPOINT = 'eastasia.api.cognitive.microsoft.com/sts/v1.0/issuetoken';
+    const response = await fetch('eastasia.api.cognitive.microsoft.com/sts/v1.0/issuetoken', {
         method: 'POST',
         headers: {
             'Ocp-Apim-Subscription-Key': '53d0e91a232e43f281b3196c8035734a',
             'Content-Type': 'audio/wav; codecs=audio/pcm; samplerate=16000',
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${token}`
+            'Accept': 'application/json'
         },
         body: audioData
     });
